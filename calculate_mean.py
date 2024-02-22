e9c7ac76fd81bb40b7ffb77c24b65214984b4b22
Roya Nasiri <rnaser917@gmail.com>
2024-02-22 13:32:24 +0100

    update docstring

--- calculate_mean.py
+++ calculate_mean.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def calculate_mean(data):
-""" This function computes the mean of an array.
+""" This function computes the mean of an array. but it's silly beacuse it uses a different function
 Input: An one dimensional array
 Output: The mean of an array
 """
